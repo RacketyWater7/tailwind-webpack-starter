9bf172147ac0293528175f0240477cafacf72388
RacketyWater7 <haseebudeen@outlook.com>
2022-07-01 10:41:56 +0500

    made the starter app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#0070f3",
+        "brand-secondary": "#0070f3",
+      },
+    },
+  },
+  plugins: [],
+}
